4173eec15f0ca45f0e6b9b9649a076e7b86d228f
Hung Bui <buingochung.ntu@gmail.com>
2022-08-07 18:19:04 +0700

    [FM-0001][Add Devise]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,20 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'mail.google.com',
+    user_name: ENV['email'],
+    password: ENV['password'],
+    authentication: 'login',
+    enable_starttls_auto: true,
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 20220806134507) do
+ActiveRecord::Schema.define(version: 20220806143054) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -8,6 +8,10 @@ ActiveRecord::Schema.define(version: 20220806134507) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
